f2e568149ac6c702226dfc63b0b3c2d5b59afc66
alyfinder <asgr@breu.io>
2024-06-27 10:50:39 +0500

    test

--- app.py
+++ app.py
@@ -4,11 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_geek():
     return """
-            <h1>Hello from Quantm test 2 repo</h1>
-            <h2>10-Feb-2024</h2>
-            <h3>Testing for #88</h3>
-            <h3>umerm64-patch-10</h3>
-            <h3>22-March-2024</h3>
+            <h1>Hello from Quantm</h1>
+            <h3>Hello from Quantm</h3>
             """
 
 
