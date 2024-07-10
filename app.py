3952f0bca65e762e76af65cd86b302ae772c9fd6
alyfinder <asgr@breu.io>
2024-07-10 14:43:16 +0500

    test

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ def hello_geek():
             <h3>Hello from Quantm</h3>
             <h3>Hello from Quantm</h3>
             <h3>Hello from Quantm</h3>
-            <h3>Hello from Quantm</h3>
 
             """
 
