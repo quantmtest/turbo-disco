2cc2020533c83e9f74f7063bbec5f12526bcabcd
alyfinder <asgr@breu.io>
2024-08-16 21:57:05 +0500

    updated

--- app.py
+++ app.py
@@ -133,7 +133,6 @@ def hello_geek():
         <h3>Hello from Quantm</h3>
         <h3>Hello from Quantm</h3>
         <h3>Hello from Quantm</h3>
-        <h3>Hello from Quantm</h3>
     """
     
 if __name__ == "__main__":
