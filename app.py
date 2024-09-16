ba03df3fbc01af475c98a745dff4441528ee0653
muhammad-asghar-ali <masgharali.eng@gmail.com>
2024-09-16 10:39:35 +0500

    test phase 2

--- app.py
+++ app.py
@@ -228,7 +228,6 @@ def hello_geek():
         <h3>Hello from Quantm</h3>   
         <h3>Hello from Quantm</h3>
         <h3>Hello from Quantm</h3>
-        <h3>Hello from Quantm</h3>   
     """
 
 
