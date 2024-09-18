2dd786bafada217bed35956ddac1ea52922b2b32
alyfinder <asgr@breu.io>
2024-09-18 15:19:37 +0500

    database

--- db.py
+++ db.py
@@ -22,3 +22,5 @@ def database():
     print("connect to database")
     print("connect to database")
     print("connect to database")
+    print("connect to database")
+    print("connect to database")
