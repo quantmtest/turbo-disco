bfccdaa23fe5b9037808ca1fd33cf880f4c80c60
muhammad-asghar-ali <masgharali.eng@gmail.com>
2024-12-11 12:33:29 +0500

    test rebase

--- q1.py
+++
@@ -1,5 +0,0 @@
-def gnt():
-    return """
-        <h2>Quantm testing</h2>
-        <h2>Quantm testing</h2>
-        """
--- qnt-new.py
+++
@@ -1,4 +0,0 @@
-def gnt():
-    return """
-        <h2>Quantm testing</h2>
-    """
